3554cf7e3be8a54a8521583150583b7d92e6e53a
Mairen <perez.ruiz.mairen@alumnat.copernic.cat>
2024-09-09 11:13:57 +0200

    Agregar archivos de proyecto.

---
+++ CodigoMorse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoMorse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Menú para el usuario
+            Console.WriteLine(@"Seleccione una opción:
+1. Convertir texto a código Morse
+2. Convertir código Morse a texto
+3. Salir");
+
+            string opciónUsuario = Console.ReadLine();
+        }
+    }
+}
